ce39927887d1dedb7cae4c94b44f71cd0334913e
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-08-28 01:25:54 -0700

    Added first two fields to widget backend

--- includes/WP-Youtube-Subscribe-scripts.php
+++ includes/WP-Youtube-Subscribe-scripts.php
@@ -7,6 +7,10 @@ function wpyts_add_scripts(){
     wp_enqueue_style('wpyts-main-style', plugins_url(). '/WP-Youtube-Subsribe/includes/css/style.css');
     //Add main js
     wp_enqueue_script('wpyts-main-script', plugins_url(). '/WP-Youtube-Subsribe/includes/js/main.js');
+
+    //Add google script
+    wp_register_script( 'google', 'https://apis.google.com/js/platform.js');
+    wp_enqueue_script( 'google' );
 }
 
 add_action( 'wp_enqueue_scripts', 'wpyts_add_scripts' );
--- includes/wpyts-subs-class.php
+++ includes/wpyts-subs-class.php
@@ -31,7 +31,12 @@ class WP_Youtbe_Subscribe_Widget extends WP_Widget {
         }
         
         //Widget Content Output
-        echo "Hello from WPTYS widget";
+        echo '<div 
+                class="g-ytsubscribe" 
+                data-channel="' . $instance['channel'] . '" 
+                data-layout="' . $instance['layout']  . '" 
+                data-count="default">
+            </div>';
         echo $args['after_widget'];//Whatever you want to display after widget (</div>)
 	}
 
@@ -44,7 +49,12 @@ class WP_Youtbe_Subscribe_Widget extends WP_Widget {
 	 */
 	public function form( $instance ) {
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Youtube Subs', 'wpyts_domain' );
-		?>
+        
+        $channel = ! empty( $instance['channel'] ) ? $instance['channel'] : esc_html__( 'wheretheybuck', 'wpyts_domain' );
+        
+        $layout = ! empty( $instance['layout'] ) ? $instance['layout'] : esc_html__( 'wheretheybuck', 'wpyts_domain' );
+        
+        ?>
         <!-- Title -->
 		<p>
 		    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">
@@ -57,6 +67,38 @@ class WP_Youtbe_Subscribe_Widget extends WP_Widget {
             name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" 
             type="text" 
             value="<?php echo esc_attr( $title ); ?>">
+		</p>
+
+        <!-- Channel -->
+		<p>
+		    <label for="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>">
+                <?php esc_attr_e( 'channel:', 'wpyts_domain' ); ?>
+            </label> 
+
+		    <input 
+            class="widefat" 
+            id="<?php echo esc_attr( $this->get_field_id( 'channel' ) ); ?>" 
+            name="<?php echo esc_attr( $this->get_field_name( 'channel' ) ); ?>" 
+            type="text" 
+            value="<?php echo esc_attr( $channel ); ?>">
+		</p>
+        <!-- Layout -->
+		<p>
+		    <label for="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>">
+                <?php esc_attr_e( 'layout:', 'wpyts_domain' ); ?>
+            </label> 
+
+		    <select 
+            class="widefat" 
+            id="<?php echo esc_attr( $this->get_field_id( 'layout' ) ); ?>" 
+            name="<?php echo esc_attr( $this->get_field_name( 'layout' ) ); ?>">
+            <option value="default" <?php echo ($layout == 'default') ? 'selected' : ''; ?>>
+                Default
+            </option> 
+            <option value="full" <?php echo ($layout == 'full') ? 'selected' : ''; ?>>
+                Full
+            </option> 
+            </select>
 		</p>
 		<?php 
 	}
@@ -74,6 +116,8 @@ class WP_Youtbe_Subscribe_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['channel'] = ( ! empty( $new_instance['channel'] ) ) ? strip_tags( $new_instance['channel'] ) : '';
+		$instance['layout'] = ( ! empty( $new_instance['layout'] ) ) ? strip_tags( $new_instance['layout'] ) : '';
 
 		return $instance;
 	}
